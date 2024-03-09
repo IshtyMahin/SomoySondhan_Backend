24dca970aaaf8701efd6c5af900ff0b9e543d2e2
IshtyMahin <ishty119@gmail.com>
2024-03-09 18:47:11 +0600

    urls setup

--- reader/views.py
+++ reader/views.py
@@ -49,7 +49,7 @@ class UserRegistrationAPIView(generics.CreateAPIView):
             print("token ",token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print("uid ",uid)
-            confirm_link = f"http://127.0.0.1:8000/user/active/{uid}/{token}"
+            confirm_link = f"https://somoysondhan-backend.onrender.com/user/active/{uid}/{token}"
             email_subject = "Confirm your Email"
             email_body = render_to_string("confirm_email.html",{"confirm_link":confirm_link})
             email = EmailMultiAlternatives(email_subject, '',to=[user.email])
@@ -68,9 +68,9 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active = True
         user.save()
-        return redirect('http://127.0.0.1:5500/login.html')
+        return redirect('https://65ec4bc538e71193265ec179--somoysondhan.netlify.app/login.html')
     else:
-        return redirect('register')
+        return redirect('https://65ec4bc538e71193265ec179--somoysondhan.netlify.app/register.html')
     
 
 class UserLoginApiView(APIView):
